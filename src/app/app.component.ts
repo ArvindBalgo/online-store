ed721f2c1b71435760324a504f3a6a95f57d32a4
rballgobin <rballgobin@maureva.com>
2024-12-14 11:34:58 +0400

    ARVIND

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
 
   redirectToWhatsApp() {
     const phoneNumber = '+23059125334'; // Replace with the recipient's phone number
-    const message = '<ul><li>2000 Cups</li><li>10 packs Banana Leafs</li></ul>>'; // Replace with your message
+    const message = "Hello,\n\nThis is a message with a new line.\n\t- This line is indented.\n\t- This is another indented line.\n\nBest regards,\nYour Name";
     const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
     window.open(url, '_blank');
   }
