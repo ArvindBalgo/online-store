9839ab40ee8bb502d90dabba09dcf2b65d213031
rballgobin <rballgobin@maureva.com>
2024-12-14 11:16:08 +0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'online-store';
+
+  redirectToWhatsApp() {
+    const phoneNumber = '+23059125334'; // Replace with the recipient's phone number
+    const message = '<ul><li>2000 Cups</li><li>10 packs Banana Leafs</li></ul>>'; // Replace with your message
+    const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank');
+  }
 }
